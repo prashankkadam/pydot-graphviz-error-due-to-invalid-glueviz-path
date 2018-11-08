0ba572d60c7c519c69638a966d3835d1c72bce95
prashank kadam <pstepper7@gmail.com>
2018-11-09 00:33:35 +0530

    Create envs.py

---
+++ envs.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 23 01:16:55 2018
+ 
+@author: prashank.kadam
+"""
+import os
+
+os.environ["PATH"] += os.pathsep + 'bin folder of the path where graphviz is installed' 
+#default('C:/Program Files (x86)/Graphviz2.38/bin')
